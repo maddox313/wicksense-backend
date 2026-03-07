0fe9ab7dd0cf9496d3763cd3b99c444b0e9653e1
maddox313 <info@comlinkglobalgroup.com>
2026-03-07 18:43:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ MARKET_SYMBOLS = {
     "DowJones": "DIA",
     "Futures": "SPY"
 }
-}
+
 
 
 # -----------------------------
@@ -770,6 +770,7 @@ def create_checkout_session():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
