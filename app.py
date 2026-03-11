194533ab320ea32b45e75f62a3a71072a6fa958e
maddox313 <info@comlinkglobalgroup.com>
2026-03-11 19:07:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1164,9 +1164,7 @@ def backtest():
         }), 500
 
 
-# -----------------------------
-# TRADE PLAN
-# -----------------------------
+
 # -----------------------------
 # TRADE PLAN
 # -----------------------------
@@ -1586,6 +1584,7 @@ def create_checkout_session():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
