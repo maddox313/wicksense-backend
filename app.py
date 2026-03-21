70971b03a277170bb1f1ec9d1bf788d78b0d21b3
maddox313 <info@comlinkglobalgroup.com>
2026-03-21 18:09:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3925,12 +3925,12 @@ def create_checkout_session():
             "details": str(e)
         }), 500
 
-seed_live_market_state()
-
-live_signal_thread = threading.Thread(target=start_twelvedata_stream, daemon=True)
-live_signal_thread.start()
-
 if __name__ == "__main__":
+    seed_live_market_state()
+
+    live_signal_thread = threading.Thread(target=start_twelvedata_stream, daemon=True)
+    live_signal_thread.start()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
